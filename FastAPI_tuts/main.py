93cb6940b189fb22d87722a458475972f9f74cf1
Raj Srivastav <48527607-rajfaltu007@users.noreply.replit.com>
2025-10-14 04:23:34 +0000

    Some changes

--- FastAPI_tuts/main.py
+++ FastAPI_tuts/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
 
+def load_data():
+  with open("patients.json", "r") as f:
+    data = json.load(f)
+    return data
+
 @app.get("/")
 def test():
-  return {"message": "Kaisan ba?"}
+  return {"message": "Patient Management System API"}
+
+@app.get("/view")
+def view():
+   data = load_data()
+   return data
